3827158dc693cb5f4e476f3e31a325bda5aceb64
Corey Stone <nx211@authoritah.com>
2025-04-11 18:02:41 -0500

    clean deploy

--- next.config.js
+++ next.config.js
@@ -5,6 +5,20 @@ const nextConfig = {
   },
   assetPrefix: process.env.NODE_ENV === 'production' ? '/' : '',
   trailingSlash: true,
+  output: 'export',
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, must-revalidate',
+          },
+        ],
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig 
